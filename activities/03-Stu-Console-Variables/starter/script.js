1d3f72cf272aad75bdd40c715b9c4c66d0beb078
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-03 22:02:50 +0100

    exercises completed

--- activities/03-Stu-Console-Variables/starter/script.js
+++ activities/03-Stu-Console-Variables/starter/script.js
@@ -1,1 +1,9 @@
 // Write Your JavaScript Code Here
+
+let personName = "First Name";
+let pets = 0;
+let funFact = "your fun fact";
+
+console.log("My name is",personName+".");
+console.log("I have",pets,"pets(s).");
+console.log("Fun fact:",funFact+".");
